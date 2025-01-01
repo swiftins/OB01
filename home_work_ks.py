9fd4c565c20b4323c93cadd9ee92b0392a3e96c2
АСУС <natalja.rejenta@gmail.com>
2025-01-01 18:03:26 +0200

    Home-work_done

--- home_work_ks.py
+++ home_work_ks.py
@@ -2,26 +2,39 @@
 #У задачи должны быть атрибуты: описание задачи, срок выполнения и статус 
 #(выполнено/не выполнено). Реализуй функцию для добавления задач, отметки выполненных 
 #задач и вывода списка текущих (не выполненных) задач.
-
 class Task:
     def __init__(self, description, time, status):
         self.description = description
         self.time = time
         self.status = status
-        self.deals = []
+        Deals.append(description)
 
     def add_task(self, task):
         self.deals.append(task)
         print(f"Добавлена задача, {task }")
 
     def mark_as_done(self):
-        self.status = "выполнено"
+        self.status = "выполнено"   
+        Deals.remove(self.description)   
+        Deals_done.append(self.description)
 
-
+Deals_done = []
+Deals = []
 
 t1 = Task("Испечь пирог", 2, "не выполнено")
+print(Deals)
 t2 = Task("Сходить в парикмахерскую", 3, "не выполнено")
+print(Deals)
 t3 = Task ("Посетить  выставку", 4 , "не выполнено")
+print(Deals)
 
-    
+print(t1.time, t2.time, t3.time)
 
+t1.mark_as_done()
+print(Deals)
+
+t2.mark_as_done()
+print(Deals)
+
+print(t1.time, t2.time, t3.time)
+print(t1.status,t2.status,t3.status)
